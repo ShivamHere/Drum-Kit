2b031db5af8dbd0474a89117c881990aa552205f
Shivam Tiwari <38392563+ShivamHere@users.noreply.github.com>
2021-04-20 00:17:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var d = document.querySelectorAll("button.drum");
-for(var i =0; i<d.length; i++)  d[i].addEventListener("click", function(){
-    var buttonInnerHtml = this.innerHTML;
+function buttonAnimation(currentKey){
+    document.querySelector('.'+currentKey).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector('.'+currentKey).classList.remove("pressed");
+    }, 100);
+}
+function makeSound(buttonInnerHtml){
     switch(buttonInnerHtml){
         case "w": new Audio('sounds/crash.mp3').play();break;
         case "a": new Audio('sounds/kick-bass.mp3').play();break;
@@ -10,23 +14,14 @@ for(var i =0; i<d.length; i++)  d[i].addEventListener("click", function(){
         case "k": new Audio('sounds/tom-3.mp3').play();break;
         case "l": new Audio('sounds/tom-4.mp3').play();break;
     }
+}
+
+var d = document.querySelectorAll("button.drum");
+for(var i =0; i<d.length; i++)  d[i].addEventListener("click", function(){
+    makeSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
 });
 document.addEventListener("keydown", function(logKey){
-    console.log(logKey);
-    switch(logKey.key){
-        case "W":
-        case "w": new Audio('sounds/crash.mp3').play();break;
-        case "A":
-        case "a": new Audio('sounds/kick-bass.mp3').play();break;
-        case "S":
-        case "s": new Audio('sounds/snare.mp3').play();break;
-        case "D":
-        case "d": new Audio('sounds/tom-1.mp3').play();break;
-        case "J":
-        case "j": new Audio('sounds/tom-2.mp3').play();break;
-        case "K":
-        case "k": new Audio('sounds/tom-3.mp3').play();break;
-        case "L":
-        case "l": new Audio('sounds/tom-4.mp3').play();break;
-    }
+    makeSound(logKey.key);
+    buttonAnimation(logKey.key);
 });
